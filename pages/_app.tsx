624f0258035623c34d3a5e0c228a6570b8576cb3
Diamank <diamankfinance@gmail.com>
2025-07-28 16:04:32 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import type { ReactNode, ReactElement } from "react";
-import type { NextPage } from "next";
+import PainelLayout from "@/components/PainelLayout";
 
-type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactNode) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-  const getLayout = Component.getLayout ?? ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <PainelLayout>
+      <Component {...pageProps} />
+    </PainelLayout>
+  );
 }
