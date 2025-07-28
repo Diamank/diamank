d8b782025c24c3d37c3b9e24fb5d8fffd65a38b7
Diamank <diamankfinance@gmail.com>
2025-07-28 11:06:32 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { Toaster } from 'sonner'
+// pages/_app.tsx
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Toaster richColors position="top-center" />
-      <Component {...pageProps} />
-    </>
-  )
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return getLayout(<Component {...pageProps} />);
 }
