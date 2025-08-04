51210acc577ce0ffb9004f5a18e45e6eccc97bbf
Diamank <diamankfinance@gmail.com>
2025-08-04 10:06:54 -0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import PainelLayout from "@/components/PainelLayout";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PainelLayout>
       <Component {...pageProps} />
+      <Toaster position="top-right" />
     </PainelLayout>
   );
 }
